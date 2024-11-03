3db032728dd081f491d2966e1ab81e75c1138dc1
Hedvaldo Costa Vieira de Oliveira <hedvaldocosta.p@gmail.com>
2024-11-03 11:22:35 -0300

    alterando arquivos

--- index.php
+++ index.php
@@ -46,3 +46,24 @@ if ($link->query($query) === TRUE) {
 ?>
 </body>
 </html>
+
+nginx.conf:
+http {
+   
+    upstream all {
+        server 172.31.0.37:80;
+        server 172.31.0.151:80;
+        server 172.31.0.149:80;
+    }
+
+    server {
+         listen 4500;
+         location / {
+              proxy_pass http://all/;
+         }
+    }
+
+}
+
+
+events { }
